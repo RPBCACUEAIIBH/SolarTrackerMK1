55bebb82594814382b25cd88e992959787dba97c
rpbcacueaiibh <Tibor ser Veres (RPBCACUEAIIBH)>
2019-03-16 03:38:04 +0200

    Potmeters, Buttons, and PWM Speed control added, serial monitoring improved.

--- ArduinoFiles/SolarTracker/Functions.ino
+++ ArduinoFiles/SolarTracker/Functions.ino
@@ -1,7 +1,59 @@
+void ReadInterface ()
+{
+  TValues[Index] = analogRead (TP) / 4;
+  NMValues[Index] = analogRead (NMP) / 4;
+  LRSValues[Index] = analogRead (LRSPEEDP) / 4;
+  UDSValues[Index] = analogRead (UDSPEEDP) / 4;
+  
+  Threshold = 0;
+  NightMode = 0;
+  LRSpeed = 0;
+  UDSpeed = 0;
+  
+  for (int i; i < Samples; i++)
+  {
+    Threshold = Threshold + TValues[i];
+    NightMode = NightMode + NMValues[i];
+    LRSpeed = LRSpeed + LRSValues[i];
+    UDSpeed = UDSpeed + UDSValues[i];
+  }
+  Threshold /= Samples;
+  NightMode /= Samples;
+  LRSpeed /= Samples;
+  UDSpeed /= Samples;
+  
+  ManLeft[1] = ManLeft[0];
+  ManLeft[0] = digitalRead (MANLEFT);
+  ManUp[1] = ManUp[0];
+  ManUp[0] = digitalRead (MANUP);
+  Auto[1] = Auto[0];
+  Auto[0] = digitalRead (AUTO);
+  ManDown[1] = ManDown[0];
+  ManDown[0] = digitalRead (MANDOWN);
+  ManRight[1] = ManRight[0];
+  ManRight[0] = digitalRead (MANRIGHT);
+
+  if (ManLeft[0] != ManLeft[1] || ManUp[0] != ManUp[1] || Auto[0] != Auto[1] || ManDown[0] != ManDown[1] || ManRight[0] != ManRight[1]) // Debouncing buttons
+  {
+    delay (5);
+    ManLeft[0] = digitalRead (MANLEFT);
+    ManUp[0] = digitalRead (MANUP);
+    Auto[0] = digitalRead (AUTO);
+    ManDown[0] = digitalRead (MANDOWN);
+    ManRight[0] = digitalRead (MANRIGHT);
+  }
+
+  if (ManLeft[0] == LOW || ManUp[0] == LOW || ManDown[0] == LOW || ManRight[0] == LOW) // You can't be in manual and automatic mode at the same time, it could potentially burn something...
+  {
+    AutoPositioning = LOW;
+    Auto[0] = HIGH;
+  }
+  if (Auto[0] == LOW && Auto[1] == HIGH && AutoPositioning == LOW) AutoPositioning = HIGH;
+  else if (Auto[0] == LOW && Auto[1] == HIGH && AutoPositioning == HIGH) AutoPositioning = LOW;
+}
+
 void ReadSensors ()
 {
-  Index += 1;
-  Index %= Samples;
   SLValues[Index] = analogRead (SL) / 4;
   SRValues[Index] = analogRead (SR) / 4;
   STValues[Index] = analogRead (ST) / 4;
@@ -29,8 +81,10 @@ void ReadSensors ()
 
 void DirectDrive () // For driving transistors and/or relays.
 {
-  if (SAvg >= NightMode)
+  if (SAvg >= NightMode && AutoPositioning == HIGH) // AutoMode
   {
+    analogWrite (LRSPEED, LRSpeed);
+    analogWrite (UDSPEED, UDSpeed);
     // Overcast (Periodic search not yet implemented...)
     // Turn RIGHT
     if (SRAvg >= SLAvg + Threshold)
@@ -70,8 +124,60 @@ void DirectDrive () // For driving transistors and/or relays.
     }
     digitalWrite (SLEEP, LOW);
   }
-  else
+  else if (AutoPositioning == LOW) // ManualMode
   {
+    if (Cycle < 256) // Blinking sleep indicator LED to indicate manual mode...
+    {
+      digitalWrite (SLEEP, HIGH);
+    }
+    else
+    {
+      digitalWrite (SLEEP, LOW);
+    }
+
+    analogWrite (LRSPEED, LRSpeed);
+    analogWrite (UDSPEED, UDSpeed);
+    // Turn RIGHT
+    if (ManRight[0] == LOW && ManLeft[0] != LOW)
+    {
+      digitalWrite (RIGHT, HIGH);
+    }
+    else
+    {
+      digitalWrite (RIGHT, LOW);
+    }
+    // Turn LEFT
+    if (ManLeft[0] == LOW && ManRight[0] != LOW)
+    {
+      digitalWrite (LEFT, HIGH);
+    }
+    else
+    {
+      digitalWrite (LEFT, LOW);
+    }
+    // Turn UP
+    if (ManUp[0] == LOW && ManDown[0] != LOW)
+    {
+      digitalWrite (UP, HIGH);
+    }
+    else
+    {
+      digitalWrite (UP, LOW);
+    }
+    // Turn DOWN
+    if (ManDown[0] == LOW && ManUp[0] != LOW)
+    {
+      digitalWrite (DOWN, HIGH);
+    }
+    else
+    {
+      digitalWrite (DOWN, LOW);
+    }
+  }
+  else // Night Mode
+  {
+    digitalWrite (LRSPEED, LOW);
+    digitalWrite (UDSPEED, LOW);
     digitalWrite (LEFT, LOW);
     digitalWrite (DOWN, LOW);
     digitalWrite (UP, LOW);
@@ -82,18 +188,175 @@ void DirectDrive () // For driving transistors and/or relays.
 
 void DataStream ()
 {
-  Serial.print ("SAvg: ");
-  Serial.print (SAvg);
-  Serial.print (" ");
-  Serial.print ("SLAvg: ");
-  Serial.print (SLAvg);
-  Serial.print (" ");
-  Serial.print ("SRAvg: ");
-  Serial.print (SRAvg);
-  Serial.print (" ");
-  Serial.print ("SBAvg: ");
-  Serial.print (SBAvg);
-  Serial.print (" ");
-  Serial.print ("STAvg: ");
-  Serial.println (STAvg);
+  switch (Cycle)
+  {
+    case 0: Sleeping = "(Zzz)";
+    break;
+    case 64: Sleeping = "(ZZz)";
+    break;
+    case 128: Sleeping = "(ZZZ)";
+    break;
+    case 192: Sleeping = "(ZZz)";
+    break;
+    case 256: Sleeping = "(Zzz)";
+    break;
+    case 320: Sleeping = "(ZZz)";
+    break;
+    case 384: Sleeping = "(ZZZ)";
+    break;
+    case 448: Sleeping = "(ZZz)";
+    break;
+  }
+
+  switch (Cycle)
+  {
+    case 0: Active = "(*  )";
+    break;
+    case 64: Active = "( * )";
+    break;
+    case 128: Active = "(  *)";
+    break;
+    case 192: Active = "( * )";
+    break;
+    case 256: Active = "(*  )";
+    break;
+    case 320: Active = "( * )";
+    break;
+    case 384: Active = "(  *)";
+    break;
+    case 448: Active = "( * )";
+    break;
+  }
+  
+  if (AutoPositioning == LOW)
+  { 
+    // Turn RIGHT Simulation
+    if (ManLeft[0] == HIGH && ManUp[0] == HIGH && ManDown[0] == HIGH && ManRight[0] == HIGH)
+    {
+      if (SRAvg >= SLAvg + Threshold) // Turn RIGHT Simulation
+      {
+        LRTurn = " R ";
+      }
+      else if (SLAvg >= SRAvg + Threshold) // Turn LEFT Simulation
+      {
+        LRTurn = " L ";
+      }
+      else
+      {
+        LRTurn = " S ";
+      }
+      if (STAvg >= SBAvg + Threshold) // Turn UP Simulation
+      {
+        UDTurn = " U ";
+      }
+      else if (SBAvg >= STAvg + Threshold) // Turn DOWN Simulation
+      {
+        UDTurn = " D ";
+      }
+      else
+      {
+        UDTurn = " S ";
+      }
+    }
+    else
+    {
+      if (ManRight[0] == LOW && ManLeft[0] != LOW) // Turn RIGHT
+      {
+        LRTurn = " MR";
+      }
+      else if (ManLeft[0] == LOW && ManRight[0] != LOW) // Turn LEFT
+      {
+        LRTurn = " ML";
+      }
+      else
+      {
+        LRTurn = " MS";
+      }
+      if (ManUp[0] == LOW && ManDown[0] != LOW) // Turn UP
+      {
+        UDTurn = " MU";
+      }
+      else if (ManDown[0] == LOW && ManUp[0] != LOW) // Turn DOWN
+      {
+        UDTurn = " MD";
+      }
+      else
+      {
+        UDTurn = " MS";
+      }
+    }
+    
+    Serial.print ("AP: Off");
+    Serial.print (";   ");
+    Serial.print ("Light: ");
+    if (SAvg < 100) Serial.print ("0");
+    if (SAvg < 10) Serial.print ("0");
+    Serial.print (SAvg);
+    Serial.print ("; ");
+    Serial.print ("Min Light: ");
+    if (NightMode < 100) Serial.print ("0");
+    if (NightMode < 10) Serial.print ("0");
+    Serial.print (NightMode);
+    Serial.print (" >> ");
+    if (SAvg < NightMode)
+    {
+      if (ManLeft[0] == HIGH && ManUp[0] == HIGH && ManDown[0] == HIGH && ManRight[0] == HIGH)
+      {
+        LRTurn = "n/a";
+        UDTurn = "n/a";
+      }
+      Serial.print (Sleeping);
+    }
+    else
+    {
+      Serial.print (Active);
+    }
+    Serial.print (";   ");
+    Serial.print ("Sensor values: ");
+    if (SLAvg < 100) Serial.print ("0");
+    if (SLAvg < 10) Serial.print ("0");
+    Serial.print (SLAvg);
+    Serial.print ("(L) ");
+    if (SRAvg < 100) Serial.print ("0");
+    if (SRAvg < 10) Serial.print ("0");
+    Serial.print (SRAvg);
+    Serial.print ("(R) ");
+    if (STAvg < 100) Serial.print ("0");
+    if (STAvg < 10) Serial.print ("0");
+    Serial.print (STAvg);
+    Serial.print ("(T) ");
+    if (SBAvg < 100) Serial.print ("0");
+    if (SBAvg < 10) Serial.print ("0");
+    Serial.print (SBAvg);
+    Serial.print ("(B) ");
+    Serial.print (">> ");
+    Serial.print (LRTurn);
+    Serial.print (" ");
+    Serial.print (UDTurn);
+    Serial.print (";   ");
+    Serial.print ("Threshold: ");
+    if (Threshold < 100) Serial.print ("0");
+    if (Threshold < 10) Serial.print ("0");
+    Serial.print (Threshold);
+    Serial.print ("; ");
+    Serial.print ("LRSpeed: ");
+    if (LRSpeed < 100) Serial.print ("0");
+    if (LRSpeed < 10) Serial.print ("0");
+    Serial.print (LRSpeed);
+    Serial.print ("; ");
+    Serial.print ("UDSpeed: ");
+    if (UDSpeed < 100) Serial.print ("0");
+    if (UDSpeed < 10) Serial.print ("0");
+    Serial.print (UDSpeed);
+    Serial.println ("");
+    DisplayString = true;
+  }
+  else
+  {
+    if (DisplayString == true)
+    {
+      Serial.println ("AP: On");
+    }
+    DisplayString = false;
+  }
 }
--- ArduinoFiles/SolarTracker/SolarTracker.ino
+++ ArduinoFiles/SolarTracker/SolarTracker.ino
@@ -1,27 +1,35 @@
-#include <EEPROM.h>
-
 // Please note that this project was made for someone with an already existing system. Had to work with he already had rather then design everything from scratch!
-// This is Version: 0.2
+// Made primarily for Arduino nano and pro mini.(The new versions of pro mini that has A6 and A7 available, but if PWM speed control is not required any older version will do as those pins only read motor speed settings from pots...)
+// This is Version: 0.3 (not fully tested)
 
 // Pins
 int SL = A0; // Left sensor
 int SR = A1; // Right sensor
 int ST = A2; // Top sensor
 int SB = A3; // Bottom sensor
-int LEFT = 2;
-int UP = 4;
-int DOWN = 7;
-int RIGHT = 8;
-int SLEEP = 13;
-// Pins Left unused: 3(pwm), 5(pwm), 6(pwm), 9(pwm), 10(pwm), 11(pwm), 12, A3, A4(I2C), A5(I2C), A6(ADC only), A7(ADC only)(Pins A6 and A7 May not be available on older arduinos.)
-// Keep the pin configuration if you're planning on updating to the newest version later!
+int LEFT = 2; // Left trun relay signal
+int UP = 4; // Up trun relay signal
+int DOWN = 7; // Down trun relay signal
+int RIGHT = 8; // Right trun relay signal
+int SLEEP = 13; // Night mode indicator
+int LRSPEED = 3; // Up-Down motor speed PWM output (Optional)
+int UDSPEED = 5; // Up-Down motor speed PWM output (Optional)
+int TP = A4; // Threshold pot
+int NMP = A5; // NightMode pot
+int LRSPEEDP = A6; // Left-Right motor speed pot (Optional, Required if PWM speed control is implemented.)
+int UDSPEEDP = A7; // Up-Down motor speed pot (Optional, Required if PWM speed control is implemented.)
+int MANLEFT = 9; // Left turn button for manual positioning (Optional, but highly recommended! Pressing it will switch to manual mode...)
+int MANUP = 10; // Up turn button for manual positioning (Optional, but highly recommended! Pressing it will switch to manual mode...)
+int AUTO = 6; // Toggle Automatic positioning (Optional, Required if either or both the Serial interface or other buttons are needed.)
+int MANDOWN = 11; // Down turn button for manual positioning (Optional, but highly recommended! Pressing it will switch to manual mode...)
+int MANRIGHT = 12; // Right turn button for manual positioning (Optional, but highly recommended! Pressing it will switch to manual mode...)
+// Use System monitor set to 115200 baud rate to read values!
 
 // Constants
 byte Samples = 128; // Valid range: 1 - 128 (128 is the maximum number of 8 bit samples that can be averaged in an integer value... Higher sample size is more accurate, but less responsive... )
-byte Threshold = 30; // Won't turn if the difference between sensors is less then this valiue (Lower this value for more, raise for less accuracy, but for high accuracy the sensors may need to be matched...)
-byte NightMode = 75; // Bbelow this value the controller does not bother turning...
 
 // Variables
+boolean AutoPositioning = HIGH;
 int Index = Samples - 1;
 unsigned int SLAvg; // Averaged Left sensor value (Measured)
 unsigned int SRAvg; // Averaged Right sensor value (Measured)
@@ -32,8 +40,28 @@ byte SLValues[128];
 byte SRValues[128];
 byte STValues[128];
 byte SBValues[128];
+unsigned int Threshold; // Won't turn if the difference between sensors is less then this valiue (Lower this value for more, raise for less accuracy, but for high accuracy the sensors may need to be matched...)
+unsigned int NightMode; // Bbelow this value the controller does not bother turning...
+unsigned int LRSpeed; // Left-Right motor speed
+unsigned int UDSpeed; // Up-Down motor speed
+byte TValues[128];
+byte NMValues[128];
+byte LRSValues[128];
+byte UDSValues[128];
+boolean ManLeft[2] = {LOW, LOW};
+boolean ManUp[2] = {LOW, LOW};
+boolean Auto[2] = {LOW, LOW};
+boolean ManDown[2] = {LOW, LOW};
+boolean ManRight[2] = {LOW, LOW};
+unsigned int Cycle = 0;
+String Active;
+String Sleeping;
+String LRTurn;
+String UDTurn;
+boolean DisplayString = false;
 
 // Functions
+void ReadInterface ();
 void ReadSensors ();
 void DirectDrive ();
 void DataStream ();
@@ -46,10 +74,20 @@ void setup()
   pinMode (UP, OUTPUT);
   pinMode (RIGHT, OUTPUT);
   pinMode (SLEEP, OUTPUT);
+  pinMode (LRSPEED, OUTPUT);
+  pinMode (UDSPEED, OUTPUT);
+  pinMode (MANLEFT, INPUT_PULLUP);
+  pinMode (MANUP, INPUT_PULLUP);
+  pinMode (AUTO, INPUT_PULLUP);
+  pinMode (MANDOWN, INPUT_PULLUP);
+  pinMode (MANRIGHT, INPUT_PULLUP);
   digitalWrite (LEFT, LOW);
   digitalWrite (DOWN, LOW);
   digitalWrite (UP, LOW);
   digitalWrite (RIGHT, LOW);
+  digitalWrite (SLEEP, LOW);
+  digitalWrite (LRSPEED, LOW);
+  digitalWrite (UDSPEED, LOW);
 
   for (int i; i < 5; i++) // Discarding first few measurements since the reference voltage may be unstable...
   {
@@ -57,6 +95,10 @@ void setup()
     SRValues[0] = analogRead (SR) / 4;
     STValues[0] = analogRead (ST) / 4;
     SBValues[0] = analogRead (SB) / 4;
+    TValues[0] = analogRead (TP) / 4;
+    NMValues[0] = analogRead (NMP) / 4;
+    LRSValues[0] = analogRead (LRSPEEDP) / 4;
+    UDSValues[0] = analogRead (UDSPEEDP) / 4;
   }
 
   for (int i; i < Samples; i++)
@@ -75,12 +117,34 @@ void setup()
   {
     SBValues[i] = analogRead (SB) / 4;
   }
+  for (int i; i < Samples; i++)
+  {
+    TValues[i] = analogRead (TP) / 4;
+  }
+  for (int i; i < Samples; i++)
+  {
+    NMValues[i] = analogRead (NMP) / 4;
+  }
+  for (int i; i < Samples; i++)
+  {
+    LRSValues[i] = analogRead (LRSPEEDP) / 4;
+  }
+  for (int i; i < Samples; i++)
+  {
+    UDSValues[i] = analogRead (UDSPEEDP) / 4;
+  }
   Serial.begin (115200);
+  Serial.println ("The display function slows down the execution, therefore it's only available in manual positioning mode. (Press the \"Auto\" button to toggle between Manual and Automatic mode.)");
 }
 
 void loop()
 {
+  Index += 1;
+  Index %= Samples;
+  ReadInterface ();
   ReadSensors ();
   DirectDrive ();
   DataStream ();
+  Cycle += 1;
+  Cycle %= 512;
 }
